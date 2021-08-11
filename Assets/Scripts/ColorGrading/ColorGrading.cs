757b944217acbf84d553bb1ebf1804f6d0e9e088
vestlee <vestleet@163.com>
2021-08-11 19:13:39 +0800

    Add HSV shader

---
+++ Assets/Scripts/ColorGrading/ColorGrading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+public class ColorGrading : MonoBehaviour
+{
+    Material ColorGradingMaterial;
+    [Range(0, 360)]
+    public int Hue = 0;
+
+    private void OnEnable()
+    {
+        ColorGradingMaterial = new Material(Shader.Find("ImageEffect/HSV"));
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, ColorGradingMaterial, 0);
+    }
+}
